887ad801dcc7e354a28f2c57ee0b53522f03b6e7
gavdniwe <dimanikus0106@gmail.com>
2021-05-27 22:40:05 +0300

    fixes

--- src/components/Main/Tokenomics/Tokenomics.tsx
+++ src/components/Main/Tokenomics/Tokenomics.tsx
@@ -3,7 +3,8 @@ import './Tokenomics.scss';
 import {TokenomicsCard} from "./TokenomicsCard/TokenomicsCard";
 import TotalSuply from '../../../assets/img/icons/icon-total-suply.svg';
 import PublicSale from '../../../assets/img/icons/icon-public-sale.svg';
-import LiquidityLocked from '../../../assets/img/icons/icon-liquidity-locked.svg';
+import PancakeUniswap from '../../../assets/img/icons/icon-pancake-uniswap.svg';
+import UniswapPancake from '../../../assets/img/icons/icon-uniswap-pancake.svg';
 import TeamTokens from '../../../assets/img/icons/icon-team-tokens.svg';
 import EcosystemDevelopment from '../../../assets/img/icons/icon-ecosystem-development.svg';
 import MarketingAdvisors from '../../../assets/img/icons/icon-marketing-advisors.svg';
@@ -14,7 +15,6 @@ import PancakeBg from '../../../assets/img/pancake-bg-m.svg';
 import Bridge from '../../../assets/img/icons/icon-bridge.svg';
 import BridgeBg from '../../../assets/img/bridge-bg-m.svg';
 import TokenomicsGraphic from '../../../assets/img/tokenomics-graphic.svg';
-import {HashLink} from "react-router-hash-link";
 
 export const Tokenomics: React.FC = () => {
     return (
@@ -22,21 +22,23 @@ export const Tokenomics: React.FC = () => {
             <div className='tokenomics__banner'>
                 <h3 className='tokenomics__banner-title text-bold text-center'>500,000,00 <span className='text-orange'> LESS Presale</span></h3>
                 <p className='tokenomics__banner-secondary text-black-l text-center'>Presale Will Be On Bounce.Finance</p>
-                <HashLink to='/#swap' className="tokenomics__banner-buy btn text-bold" smooth>Buy  LESS token</HashLink>
+                <a href="https://app.bounce.finance/fixed-swap/5864" target="_blank" rel="noreferrer noopener" className="tokenomics__banner-buy btn text-bold">Buy  LESS token</a>
             </div>
             <div className="container" id='tokenomics'>
                 <h2 className="h1 text-bold text-center">Tokenomics</h2>
                 <div className='tokenomics__about'>
                     <div className="tokenomics__cards">
-                        <TokenomicsCard picture={TotalSuply} title={'Total Supply: '} amount='1,250,000,000'/>
-                        <TokenomicsCard picture={PublicSale} title={'Public Sale: '} amount='500,000,000'/>
-                        <TokenomicsCard picture={LiquidityLocked} title={'Liquidity Locked: '} amount='187,500,000'
+                        <TokenomicsCard picture={TotalSuply} title={'Total Supply: '} amount='125,000,000' sub="LESS"/>
+                        <TokenomicsCard picture={PublicSale} title={'Presale: '} amount='50,000,000' sub="LESS 40%"/>
+                        <TokenomicsCard picture={UniswapPancake} title={'Uniswap Liquidy: '} amount='12,500,000' sub="LESS 10%"
                                         className='left-zero'/>
-                        <TokenomicsCard picture={TeamTokens} title={'Team Tokens: '} amount='50,000,000'/>
-                        <TokenomicsCard picture={EcosystemDevelopment} title={'Ecosystem Development: '}
-                                        amount='425,000,000'/>
-                        <TokenomicsCard picture={MarketingAdvisors} title={'Marketing & Advisors : '}
-                                        amount='87,500,000'/>
+                        <TokenomicsCard picture={PancakeUniswap} title={'PancakeSWAP: '} amount='6,250,000' sub="LESS 5%"
+                                        className='left-zero'/>
+                        <TokenomicsCard picture={TeamTokens} title={'Team:'} amount='5,000,000' sub="LESS 4% LOCKED 1 Year"/>
+                        <TokenomicsCard picture={EcosystemDevelopment} title={'Marketing: '}
+                                        amount='8,750,000' sub="LESS 7% Locked 3 Months"/>
+                        <TokenomicsCard picture={MarketingAdvisors} title={'Ecosystem (Products) '}
+                                        amount='42,500,000' sub="34% Locked"/>
                     </div>
                     <div className="tokenomics__graphic">
                         <div className='tokenomics__graphic-bg'></div>
--- src/components/Main/Tokenomics/TokenomicsCard/TokenomicsCard.tsx
+++ src/components/Main/Tokenomics/TokenomicsCard/TokenomicsCard.tsx
@@ -5,17 +5,18 @@ interface TokenomicsCardProps {
     picture: string;
     title: string;
     amount: string;
+    sub: string;
     className?:string;
 }
 
-export const TokenomicsCard: React.FC<TokenomicsCardProps> = ({picture, title, amount,className}) => {
+export const TokenomicsCard: React.FC<TokenomicsCardProps> = ({picture, title, amount,sub,className}) => {
     return (
         <div className="tokenomics-card">
             <img src={picture} className={`${className} tokenomics-card__img`} alt='icon'/>
             <div className='tokenomics-card__content'>
                 <h4 className='tokenomics-card__title text-blue text-bold'>{title}</h4>
                 <p className='tokenomics-card__amount text-regular'>{amount}</p>
-                <p className='tokenomics-card__less text-orange text-bold'>LESS</p>
+                <p className='tokenomics-card__less text-orange text-bold'>{sub}</p>
             </div>
         </div>
     )
